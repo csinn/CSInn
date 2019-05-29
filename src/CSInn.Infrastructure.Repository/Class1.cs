be43bd592154c4aef8b75e53fa3cc7b057234861
Trinitrotoluene <pmichallet00@gmail.com>
2019-05-29 09:50:26 +0200

    Clean up comments & other auto-genned code. Add a reference to Discord.OAuth2 in the solution file.

--- src/CSInn.Infrastructure.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSInn.Infrastructure.Repository
-{
-    public class Class1
-    {
-    }
-}
--- src/CSInn.Presentation.Blazor/Startup.cs
+++ src/CSInn.Presentation.Blazor/Startup.cs
@@ -14,8 +14,6 @@ namespace CSInn.Presentation.Blazor
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -23,7 +21,6 @@ namespace CSInn.Presentation.Blazor
             services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -32,7 +29,6 @@ namespace CSInn.Presentation.Blazor
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
