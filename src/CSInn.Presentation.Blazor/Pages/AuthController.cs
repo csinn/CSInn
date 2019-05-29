1095b20275324f5cf98737450647750a261688b9
Trinitrotoluene <pmichallet00@gmail.com>
2019-05-29 12:53:25 +0200

    Auth controller to handle login/logout actions.

---
+++ src/CSInn.Presentation.Blazor/Pages/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CSInn.Presentation.Blazor.Pages
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("login"), HttpGet]
+        public IActionResult Login(string redirectUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl });
+        }
+
+        // TODO: This controller isn't mapped for some reason.
+        [Route("logout"), HttpGet]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+
+            return RedirectToAction("/");
+        }
+    }
+}
