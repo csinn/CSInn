8caaa6f2668bb31fc9a72239b1c9f0a0e9eaa330
Trinitrotoluene <pmichallet00@gmail.com>
2019-05-31 00:16:44 +0200

    Move OAuth configuration into an extension method.

---
+++ src/CSInn.Authentication/DiscordAuthenticationDefaults.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSInn.Application.Discord.Authentication
+{
+    public static class DiscordAuthenticationDefaults
+    {
+        public static string AuthenticationScheme { get; } = "Discord";
+    }
+}
---
+++ src/CSInn.Authentication/OAuthExtensions.cs
@@ -0,0 +1,56 @@
+﻿using CSInn.Application.Discord.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OAuthExtensions
+    {
+        public static AuthenticationBuilder AddDiscord(this AuthenticationBuilder builder, IConfiguration config)
+        {
+            builder.AddOAuth(DiscordAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.ClientId = config["discord:client_id"];
+                options.ClientSecret = config["discord:app_secret"];
+                options.CallbackPath = new PathString("/signin-discord");
+
+                options.AuthorizationEndpoint = "https://discordapp.com/api/oauth2/authorize";
+                options.TokenEndpoint = "https://discordapp.com/api/oauth2/token";
+                options.UserInformationEndpoint = "https://discordapp.com/api/users/@me";
+
+                options.Scope.Add("identify");
+
+                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id", ClaimValueTypes.UInteger64);
+                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "username", ClaimValueTypes.String);
+                options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email", ClaimValueTypes.Email);
+                options.ClaimActions.MapJsonKey("urn:discord:discriminator", "discriminator", ClaimValueTypes.UInteger32);
+                options.ClaimActions.MapJsonKey("urn:discord:avatar", "avatar", ClaimValueTypes.String);
+                options.ClaimActions.MapJsonKey("urn:discord:verified", "verified", ClaimValueTypes.Boolean);
+
+                options.Events = new OAuthEvents()
+                {
+                    OnCreatingTicket = async ctx =>
+                    {
+                        var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+
+                        var response = await ctx.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, ctx.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        var document = await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+                        ctx.RunClaimActions(document.RootElement);
+                    }
+                };
+            });
+
+            return builder;
+        }
+    }
+}
