4162f625f877f2384b7a7c827f8ae3d532c466ca
MobyMon3060 <50107662+MobyMon3060@users.noreply.github.com>
2019-06-14 14:22:56 +0200

    Added the ability to edit your post
    You can edit your post and click submit but for now it takes you to an empty page.

---
+++ src/CSInn.Presentation.Blazor/CodePostClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+namespace CSInn.Presentation.Blazor
+{    public class CodePostClass
+    {
+        public static void main(String[]args)
+        {
+            ArrayList Posts = new ArrayList();
+        }
+    }
+}
